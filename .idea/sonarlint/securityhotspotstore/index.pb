92e2017225d3dcfe589c41d9d220bec1ab915cea
Minhnv <nvminhhust@gmail.com>
2024-02-13 17:04:04 +0700

    feat: create model product

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,11 @@ L
 src/helpers/check.connect.js,c\b\cb0d527caf6a6116afd526eb64d4998924b656e1
 9
 	server.js,d\c\dc60ea9ee1ff6246bf342463277d5b914db2883d
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+F
+src/db/init.mongodb.js,2\0\2048af42404ccf3db613090d073cfe62343c9b48
+H
+src/models/shop.model.js,e\8\e8f5cdd9aef73ac81cca6212976ce84835c1758d
+K
+src/models/product.model.js,6\a\6a70a9327bc62ad2ffe30ef94ac7f95d1c85660f
---
+++ src/models/product.model.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const {model, Schema, Types} = require('mongoose')
+
+const DOCUMENT_NAME = 'Product'
+const COLLECTION_NAME = 'Products'
+const PRODUCT_TYPES = ['Electronics', 'Clothing', 'Furniture']
+
+const productSchema = new Schema({
+    product_name: {type: String, required: true},
+    product_thumbnail: {type: String, required: true},
+    product_description: String,
+    product_price: {type: Number, required: true},
+    product_quantity: {type: Number, required: true},
+    product_type: {type: String, required: true, enum: PRODUCT_TYPES},
+    product_shop: String,
+    product_attributes: {type: Schema.Types.Mixed, required: true}
+}, {
+    collection: COLLECTION_NAME,
+    timestamps: true
+})
+
+
+// define type product 'clothing'
+const clothingSchema = new Schema({
+    brand: {type: String, required: true},
+    size: String,
+    material: String
+}, {
+    collection: 'clothes',
+    timestamps: true
+})
+
+// define type product 'electronics'
+const electronicSchema = new Schema({
+    manufacturer: {type: String, required: true},
+    model: String,
+    color: String
+}, {
+    collection: 'electronics',
+    timestamps: true
+})
+
+module.exports = {
+    Product: model(DOCUMENT_NAME, productSchema),
+    Clothing: model('Clothing', clothingSchema),
+    Electronic: model('Electronic', electronicSchema)
+}
